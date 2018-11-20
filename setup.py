230f23a9e9bd82e31ac0638707e1942fdb9ae720
test.test <test@test.com>
2018-11-20 20:51:57 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fourth_git',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='user',
+    author_email='',
+    description=''
+)
